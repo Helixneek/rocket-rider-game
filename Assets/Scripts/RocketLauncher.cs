90db009869f74907f2d0c7471704a033da5bc219
Nico Marcelino <nico.the4thkid@gmail.com>
2023-09-25 10:11:26 +0700

    Added the initial project files
    Rocket propulsion system included

---
+++ Assets/Scripts/RocketLauncher.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+    public Transform launcherTransform;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    public float fireRate = 0.2f;
+    public float nextFireTime = 0f;
+
+    private float resetInterval = 300f; // Reset the timer every 5 minutes, to avoid gradual floating point precision loss
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && Time.time >= nextFireTime)
+        {
+            ShootRocket();
+        }
+
+        RotateLauncher();
+
+        ResetCooldown();
+    }
+
+    private void ResetCooldown()
+    {
+        if (Time.time >= nextFireTime + resetInterval)
+        {
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    private void RotateLauncher()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan2(
+            mousePosition.y - launcherTransform.position.y,
+            mousePosition.x - launcherTransform.position.x
+        ) * Mathf.Rad2Deg;
+
+        launcherTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    private void ShootRocket()
+    {
+        // Get the mouse position in world coordinates
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Calculate the direction from the shooting point to the mouse cursor
+        Vector2 direction = (mousePosition - transform.position).normalized;
+        direction.Normalize();
+
+        // Instantiate the bullet
+        GameObject bullet = Instantiate(bulletPrefab, launcherTransform.position, Quaternion.identity);
+
+        // Apply the calculated direction as the initial velocity
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        Debug.Log("Direction: " + direction);
+        Debug.Log("Direction + Speed: " + direction * bulletSpeed);
+
+        // Update the next allowed shot time
+        nextFireTime = Time.time + fireRate;
+    }
+}
---
+++ Assets/Scripts/RocketObject.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketObject : MonoBehaviour
+{
+    public GameObject explosionPrefab;    // Reference to the explosion prefab
+    public float explosionForce = 10f;    // Force of the explosion
+    public float explosionRadius = 2f;    // Radius of the explosion
+    public float explosionDuration = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the bullet collided with a wall
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Get destroyed");
+            // Play particle effects
+
+            // Apply force to surrounding rigidbodies
+            PushObjectsInRadius(transform.position);
+
+            // Destroy the bullet when it collides with a wall
+            Destroy(gameObject);
+        }
+    }
+    private void CreateExplosion(Vector2 position)
+    {
+        GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        Destroy(explosion, explosionDuration); // Destroy the explosion after 1 second (adjust as needed)
+    }
+
+    private void PushObjectsInRadius(Vector2 position)
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, explosionRadius);
+
+        foreach (Collider2D collider in colliders)
+        {
+            Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                // Calculate the direction from the explosion center to the object
+                Vector2 direction = (rb.position - position).normalized;
+
+                // Apply force to the object
+                rb.AddForce(direction * explosionForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
