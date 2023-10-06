21df7e478fb6a0ff467739d6bf934e4a8d6d5912
ReXIGnoTuS <33598331+DelUgleoDiablo@users.noreply.github.com>
2023-10-06 21:26:05 +0700

    Main Menu - Lv Selection
    Play button
    Quit
    Reset Player Refs
    Unlock Lock system Level

--- Assets/Scripts/Level Load/changeScene.cs
+++ Assets/Scripts/Level Load/changeScene.cs
@@ -7,16 +7,26 @@ using UnityEngine.UIElements;
 
 public class changeScene : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-
-            // Make the player GameObject persist across scene changes
+            UnlockNewLevel();
             sceneController.instance.NextLevel();
         }
     }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
 }
+
+
 
 
